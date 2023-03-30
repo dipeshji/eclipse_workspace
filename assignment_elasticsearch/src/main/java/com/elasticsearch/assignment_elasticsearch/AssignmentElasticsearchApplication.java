5e44c879f550f6b718f81939a68a4da1e7fbbd02
Dipesh Patidar <dpatidar336@gmail.com>
2023-03-30 12:12:55 +0530

    feat: added elastic search assignment

---
+++ assignment_elasticsearch/src/main/java/com/elasticsearch/assignment_elasticsearch/AssignmentElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.elasticsearch.assignment_elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentElasticsearchApplication.class, args);
+	}
+
+}
---
+++ assignment_elasticsearch/src/test/java/com/elasticsearch/assignment_elasticsearch/AssignmentElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elasticsearch.assignment_elasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentElasticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
